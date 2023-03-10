f924662d3ba63fa00228114bb1c85dc901b1e6f5
Ezequiel Rango <ERango@proveedor.teco.com.ar>
2023-03-10 13:17:06 -0300

    add styles in the components

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="entry-container mb-3 pointer p-2">
+
+        <div class="entry-title d-flex">
+            <span class="text-success">10</span>
+            <span class="mx-1 fs-5">Marzo</span>
+            <span class="mx-2 fw-light">2023</span>
+        </div>
+
+        <div class="entry-description">
+            Sint quis proident consectetur ullamco ullamco. Ipsum veniam nulla excepteur nisi pariatur fugiat. Ut excepteur magna id mollit do cillum ipsum veniam.
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+ .entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover {
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry-description {
+        font-size: 12px;
+    }
+}
+</style>
