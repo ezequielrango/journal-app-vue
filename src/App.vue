b05f0bf151af6fa51d9c7273fb5977d60ccce291
Ezequiel Rango <ERango@proveedor.teco.com.ar>
2023-03-10 12:29:57 -0300

    add sass, modules and components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+// import Home from './views/Home.vue'
 </script>
 
 <template>
+  <Home/>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
   <RouterView />
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import dayBookRouter from '../modules/daybook/router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -15,6 +15,10 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       // component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/daybook',
+      ...dayBookRouter
     }
   ]
 })
