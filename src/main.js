c3004afddcd6c7cb5613728605001249fbfe8730
Ezequiel Rango <ERango@proveedor.teco.com.ar>
2023-03-14 12:40:03 -0300

    ,

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+
 import './styles/styles.scss'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+    .use( store )
+    .use( router )
+    .mount('#app')
--- src/store/module-template/mutations.js
+++ src/store/module-template/mutations.js
@@ -1,2 +1,4 @@
-// export const myMutation = (state) => {
+
+// export const myAction = ( state ) => {
+
 // }
