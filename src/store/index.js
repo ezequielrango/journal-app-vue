864c3f1e3922a307bdcb90ac9e62761f9965a745
Ezequiel Rango <ERango@proveedor.teco.com.ar>
2023-03-28 11:09:04 -0300

    create auth store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 import { createStore } from 'vuex'
 
 import journal from '../modules/daybook/store/journal'
-
+import auth from '../modules/auth/store/index'
 
 const store = createStore({
     modules: {
-        journal
+        journal,
+        auth
     }
 })
 
