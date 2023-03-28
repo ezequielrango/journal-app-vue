8c58eee926ddabb815287580eadecc286e50b0c6
Ezequiel Rango <ERango@proveedor.teco.com.ar>
2023-03-28 11:29:06 -0300

    manejo del form y authComposable

--- src/modules/auth/views/Register.vue
+++ src/modules/auth/views/Register.vue
@@ -2,13 +2,13 @@
 
 
   <span class="login100-form-title p-b-41"> Registrarse </span>
-  <form class="login100-form validate-form p-b-33 p-t-5">
+  <form @submit.prevent="onSubmit" class="login100-form validate-form p-b-33 p-t-5">
     <div class="wrap-input100 validate-input" data-validate="Ingrese su nombre">
-      <input class="input100" type="text" placeholder="Correo" required />
+      <input v-model="userForm.name" class="input100" type="text" placeholder="Nombre" required />
       <span class="focus-input100" data-placeholder="&#xe82a;"></span>
     </div>
     <div class="wrap-input100 validate-input" data-validate="Ingrese email">
-      <input class="input100" type="text" placeholder="Correo" required />
+      <input v-model="userForm.email" class="input100" type="email" placeholder="Correo" required />
       <span class="focus-input100" data-placeholder="&#xe818;"></span>
     </div>
 
@@ -17,13 +17,14 @@
         class="input100"
         type="password"
         placeholder="Contraseña"
-        required
+        required 
+        v-model="userForm.password"
       />
       <span class="focus-input100" data-placeholder="&#xe80f;"></span>
     </div>
 
     <div class="container-login100-form-btn m-t-32">
-      <button class="login100-form-btn">Crear cuenta</button>
+      <button type="submit" class="login100-form-btn">Crear cuenta</button>
     </div>
 
     <div class="container-login100-form-btn m-t-32">
@@ -35,7 +36,29 @@
 </template>
 
 <script>
-export default {};
+import { ref } from 'vue';
+import useAuth from '../composables/useAuth'
+
+export default {
+  setup(){
+
+    const {createUser} = useAuth()
+
+    const userForm = ref({
+      name : '',
+      email: '',
+      password: '',
+    })
+
+
+    return{
+      userForm,
+      onSubmit : async () => {
+        createUser(userForm.value);
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
