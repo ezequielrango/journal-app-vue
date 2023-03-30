1edad5dd068535b17e4824830501189bbb171c35
Ezequiel Rango <ezequielrango29@gmail.com>
2023-03-30 17:10:54 -0300

    add interceptors in the authAPI

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -6,4 +6,10 @@ const journalApi = axios.create({
     baseURL: 'https://vue-demo-d43c0-default-rtdb.firebaseio.com'
 })
 
+journalApi.interceptors.request.use( (config) => {
+    config.params = {
+        auth: localStorage.getItem('idToken')
+    }
+})
+
 export default journalApi
